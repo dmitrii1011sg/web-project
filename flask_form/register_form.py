a170bbf00c28b11967c4e527a0106449bab1b543
dmitrii <dmitrii1011sg@gmail.com>
2022-10-29 11:14:34 +0300

    fix bug in register form

--- flask_form/register_form.py
+++ flask_form/register_form.py
@@ -5,8 +5,8 @@ from flask_wtf.file import FileField
 
 
 class RegisterForm(FlaskForm):
-    login = StringField('Login')
-    phone_number = StringField('Phone number')
+    login = StringField('Login', validators=[DataRequired()])
+    phone_number = StringField('Phone number', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_again = PasswordField('Repeat password', validators=[DataRequired()])
     roles = StringField('Roles')
